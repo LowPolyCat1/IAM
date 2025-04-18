c4555344881534c3244faf6dbd81348d3d6fa5a2
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-04-18 22:42:52 +0200

    added register endpoint

--- src/server.rs
+++ src/server.rs
@@ -115,19 +115,6 @@ fn parse_server_port(server_port_string: &str) -> u16 {
     };
 }
 
-#[post("/register")]
-async fn register(req: web::Json<RegisterRequest>, data: web::Data<AppState>) -> impl Responder {
-    let db = &data.db;
-    let username = req.username.clone();
-    let password = req.password.clone();
-    let email = req.email.clone();
-
-    match db.register(username, password, email).await {
-        Ok(message) => HttpResponse::Ok().body(message),
-        Err(error) => HttpResponse::InternalServerError().body(format!("Error: {}", error)),
-    }
-}
-
 #[get("/ping")]
 async fn ping() -> impl Responder {
     "pong"
