a68e6d6fc0daaed4f5ba1e3b5a0916ab5d51d99d
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-04-19 16:42:32 +0200

    modified the tests to run the correct functions also removed deprecated hash_email

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,24 +1,15 @@
 #[cfg(test)]
 mod hashing_tests {
-    use crate::hashing::hash;
-
-    #[test]
-    fn test_hash_email() {
-        let email = "t@example.com".to_string();
-        let result = hash(&email);
-        assert!(result.is_ok());
-        let hash = result.unwrap();
-        assert_eq!(hash.0.len(), 97); // Aragon2 hash length is 96 characters
-    }
+    use crate::hashing::hash_random_salt;
 
     #[test]
     fn test_hash_password() {
         let password = "password".to_string();
-        let result = hash(&password);
+        let result = hash_random_salt(&password);
         assert!(result.is_ok());
         let hash = result.unwrap();
-        assert!(!hash.0.is_empty());
-        assert!(!hash.1.is_empty());
+        assert!(!hash.is_empty());
+        assert!(!hash.is_empty());
     }
 }
 
@@ -61,8 +52,8 @@ mod encryption_tests {
         let key_bytes: [u8; 32] = [0u8; 32];
         let plaintext = "This is a secret message.";
 
-        let encrypted = encrypt_with_random_nonce(&key_bytes, plaintext);
-        let decrypted = decrypt_with_nonce(&key_bytes, &encrypted);
+        let encrypted = encrypt_with_random_nonce(&key_bytes, plaintext).unwrap();
+        let decrypted = decrypt_with_nonce(&key_bytes, &encrypted).unwrap();
 
         assert_eq!(plaintext, decrypted);
 
