13652da6f494d48d5ae43c070174e018124cc854
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-04-21 23:30:56 +0200

    docker will now continue to run in hte background

--- run_docker.py
+++ run_docker.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     run_command = [
         "docker",
         "run",
+        "-d",
         "-p",
         f"{server_ip}:{server_port}:{docker_exposed_port}",
         image_name,
